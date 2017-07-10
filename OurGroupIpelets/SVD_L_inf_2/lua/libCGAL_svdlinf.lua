3461be15ec81f25770d4d1a8feb98937c81a0ff6
falcon <aron.fiechter@gmail.com>
2017-07-10 19:45:41 +0200

    Added SVD_L_inf_2

---
+++ OurGroupIpelets/SVD_L_inf_2/lua/libCGAL_svdlinf.lua
@@ -0,0 +1,27 @@
+----------------------------------------------------------------------
+-- CGAL svdlinf ipelet description
+----------------------------------------------------------------------
+
+label = "SVDLinf"
+
+about = [[
+This ipelet is based on the CGAL_ipelet package. See www.cgal.org.
+]]
+
+-- this variable will store the C++ ipelet when it has been loaded
+ipelet = false
+
+function run(model, num)
+  if not ipelet then ipelet = assert(ipe.Ipelet(dllname)) end
+  model:runIpelet(methods[num].label, ipelet, num)
+end
+
+methods = {
+  { label="Segment VD Linf general" },
+  { label="Segment skeleton Linf general" },
+  { label="Segment VD Linf axis-parallel" },
+  { label="Segment skeleton Linf axis-parallel" },
+  { label="Help" },
+}
+
+----------------------------------------------------------------------
